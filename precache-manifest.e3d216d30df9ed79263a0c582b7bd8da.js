8e85091cdcf47bba8c388a2a9bfe2668ccd309c6
aemperador <emperadoragustin@gmail.com>
2020-05-20 03:46:31 -0300

    primer commit

---
+++ precache-manifest.e3d216d30df9ed79263a0c582b7bd8da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0010d95c8783d372a716dc84adfa1672",
+    "url": "/monster-rollodex/index.html"
+  },
+  {
+    "revision": "67e6a5c7a0c2aaeacd70",
+    "url": "/monster-rollodex/static/css/main.9c985840.chunk.css"
+  },
+  {
+    "revision": "e0437b2a4c2c48b4faac",
+    "url": "/monster-rollodex/static/js/2.e85c222a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rollodex/static/js/2.e85c222a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e6a5c7a0c2aaeacd70",
+    "url": "/monster-rollodex/static/js/main.da2c316a.chunk.js"
+  },
+  {
+    "revision": "567e4d14994c96dc03e5",
+    "url": "/monster-rollodex/static/js/runtime-main.34eac548.js"
+  }
+]);
